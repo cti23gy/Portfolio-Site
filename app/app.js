7beb49cfffa05caae72176ec7ce39605a773ab04
cti23gy <70219724+cti23gy@users.noreply.github.com>
2021-10-17 14:09:02 -0400

    Initial commit

---
+++ app/app.js
@@ -0,0 +1,62 @@
+function route() {
+    let hashTag = window.location.hash;
+    let pageID = hashTag.replace("#/", "");
+    
+    if(!pageID){
+        navToPage("home");
+    } else if (pageID == "product1") {
+        $.get(`pages/productlist/${pageID}.html`, function(data) {
+            console.log(data);
+            $("#app").html(data);
+        });
+    } else if (pageID == "product2") {
+        $.get(`pages/productlist/${pageID}.html`, function(data) {
+            console.log(data);
+            $("#app").html(data);
+        });
+    } else if (pageID == "product3") {
+        $.get(`pages/productlist/${pageID}.html`, function(data) {
+            console.log(data);
+            $("#app").html(data);
+        });
+    } else if (pageID == "product4") {
+        $.get(`pages/productlist/${pageID}.html`, function(data) {
+            console.log(data);
+            $("#app").html(data);
+        });
+    } else if (pageID == "product5") {
+        $.get(`pages/productlist/${pageID}.html`, function(data) {
+            console.log(data);
+            $("#app").html(data);
+        });
+    } else if (pageID == "product6") {
+        $.get(`pages/productlist/${pageID}.html`, function(data) {
+            console.log(data);
+            $("#app").html(data);
+        });
+    } else {
+        navToPage(pageID);
+    }
+}
+
+function navToPage(pageName) {
+    $.get(`pages/${pageName}/${pageName}.html`, function(data) {
+        console.log(data);
+        $("#app").html(data);
+    });
+}
+
+function navToProductList(pageName) {
+    
+}
+
+function initListeners() {
+    $(window).on("hashchange", route);
+    route();
+}
+
+$(document).ready(function() {
+    initListeners();
+    //navToPage("home");
+    
+});
